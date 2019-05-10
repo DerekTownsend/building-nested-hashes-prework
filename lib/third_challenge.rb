d508a39290de13e320cb475537a6362ebaccced5
Derek Townsend <derekjavon2@gmail.com>
2019-05-10 04:31:27 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -49,14 +49,14 @@ def third_challenge
         },
       :heroine_friends => [
         {
-            name:"",
-            age:"",
-            attitude:""
+            name:"Steven",
+            age:"30",
+            attitude:"confused"
         },
         {
-            name:"",
-            age:"",
-            attitude:""
+            name:"Nurse",
+            age:"44",
+            attitude:"worried"
         }
       ]
    }
